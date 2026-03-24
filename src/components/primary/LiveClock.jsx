57ac39d20162c9c715ec2b92aa09bfdd50ba3423
Nhi Tran <nhigt@uci.edu>
2026-03-23 22:30:01 -0700

    added live clock

--- src/components/primary/LiveClock.jsx
+++ src/components/primary/LiveClock.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+
 function LiveClock () {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(()=> {
+        const timer = setInterval(()=>{
+            setCurrentTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timer);
+
+    }, []);
+
     return (
         <div>
-            it's currently [time]
+            it's currently {currentTime.toLocaleTimeString()}
         </div>
     );
 }
